cc96e3888f4aa8ee284fd3c1de79bb539bb29129
Carlos Arévalo <45299446+carlosv20@users.noreply.github.com>
2020-03-14 10:38:29 -0500

    Add files via upload
    Created

---
+++ WEB/Webprueba/Webprueba/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webprueba.Models
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+
+        public char Nombre { get; set; }
+
+        public char Apellido { get; set; }
+    }
+}
---
+++ WEB/Webprueba/Webprueba/Models/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webprueba.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int ProductoID { get; set; }
+
+        public char Nombre { get; set; }
+
+        public int Cantidad { get; set; }
+
+    }
+}
